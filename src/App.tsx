d8f1f8e4729131786e33227a4e2598f095db36b4
Kaio Fonseca <mkaiofonseca@gmail.com>
2023-02-07 15:53:43 -0300

    part 2(Quase finalizando)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import * as C from './App.styles'
 import { Item } from './types/Item';
 import { ListItem } from './components/ListItem'
+import { AddArea } from './components/AddArea'
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
@@ -14,7 +15,7 @@ const App = () => {
       <C.Area>
         <C.Header> Lista de Tarefas </C.Header>
 
-        {/* Area de add nova tarefa */}
+        <AddArea />
 
         {list.map((item, index) => (
           <ListItem key={index} item={item}/>
---
+++ src/components/AddArea/index.tsx
@@ -0,0 +1,26 @@
+import { useState, KeyboardEvent } from 'react'
+import * as C from './styles'
+
+export const AddArea = () => {
+    const [inputText, setInputText] = useState('')
+
+    const handleKeyUp = (e: KeyboardEvent) => {
+        if(e.code === 'Enter' && inputText !== '') {
+            
+        }
+    }
+
+  return (
+    <C.Container>
+        <div className="image">➕</div>
+        <input 
+            type="text"
+            placeholder='Adicione uma Tarefa'
+            value={inputText}
+            onChange={e => setInputText(e.target.value)}
+            onKeyUp={handleKeyUp}
+        />
+    </C.Container>
+  )
+}
+
